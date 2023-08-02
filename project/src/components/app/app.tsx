bb200359c7c6e97da133bddb01622b933f286557
Лазарев Е <lazarev@tprs.ru>
2023-08-02 17:00:59 +0300

    Добавит компонент PrivateRoute

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,12 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
 import OfferScreen from '../../pages/offer-screen/offer-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import ScrollToTop from '../scroll-to-top/scroll-to-rop';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   offersCount: number;
@@ -27,7 +28,13 @@ function App({offersCount}: AppScreenProps): JSX.Element {
           />
           <Route
             path={AppRoute.Favorites}
-            element={<FavoritesScreen />}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthorizationStatus.NoAuth}
+              >
+                <FavoritesScreen />
+              </PrivateRoute>
+            }
           />
           <Route
             path={AppRoute.Room}
@@ -35,7 +42,7 @@ function App({offersCount}: AppScreenProps): JSX.Element {
           />
         </Route>
         <Route
-          path="*"
+          path={AppRoute.NotFound}
           element={<NotFoundScreen />}
         />
       </Routes>
